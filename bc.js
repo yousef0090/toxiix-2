3438fc82b53491a9c489b2071c22acd6d97c7a03
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-23 02:34:02 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "-";
+var prefix = "+";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : ixVolBiNa ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Dynamic , -bc`,"http://twitch.tv/S-F")
+client.user.setGame(`.Mσe ❥ ,+bc`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
